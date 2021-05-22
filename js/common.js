e8936428bca7c1170622858bf47b8b7d28623561
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-23 01:00:08 +0900

    mainpage update
    mainpage update

--- js/common.js
+++ js/common.js
@@ -1,13 +1,64 @@
 $(function () {
+    // gnb event
     $('#gnb>ul>li').mouseenter(function () {
-        $(this).find('.view').css('display','block')
-        $(this).find('.view').stop().animate({opacity : '1'},200)
-
+        $(this).find('.view').css('display', 'block')
+        $(this).find('.view').stop().animate({ opacity: '1' }, 200)
     }).mouseleave(function () {
         $(this).find('.view').css({
-            'display':'none',
-            'opacity':'0',
-    });
+            'display': 'none',
+            'opacity': '0',
+        });
+    })
 
+    // mobile event
+    let mNewReleases = $('.mNew_Releases');
+    $('.mgnb_btn').click(function () {
+        // body scroll stop
+        $('body').toggleClass('body_scroll');
+
+        // scroll bar width padding
+        $('.hbottom.container').toggleClass('hbottom_on');
+
+        // blur effect
+        $('section').toggleClass('blur')
+
+        // mobile btn span event 
+        $('.mgnb_btn a span:nth-child(1)').toggleClass('first_bar');
+        $('.mgnb_btn a span:nth-child(2)').toggleClass('opacity_zero');
+        $('.mgnb_btn a span:nth-child(3)').toggleClass('last_bar');
+
+        // mobile gnb 
+        if ($('.gnb').hasClass('mgnb_on') == true || $('.mNew_Releases').hasClass('mNew_on') == true) {
+            $('.mgnb').toggleClass('mgnb_on');
+            mNewReleases.toggleClass('mNew_on');
+        } else {
+            $('.mgnb').toggleClass('mgnb_on');
+        }
+    })
+
+    // section click removeClass mgnb
+    $('section').click(function () {
+        // body scroll stop
+        $('body').removeClass('body_scroll');
+
+        // scroll bar width padding
+        $('.hbottom.container').removeClass('hbottom_on');
+
+        // blur effect
+        $('section').removeClass('blur');
+
+        // mobile btn span event 
+        $('.mgnb_btn a span:nth-child(1)').removeClass('first_bar');
+        $('.mgnb_btn a span:nth-child(2)').removeClass('opacity_zero');
+        $('.mgnb_btn a span:nth-child(3)').removeClass('last_bar');
+
+        // mobile gnb
+        $('.mgnb').removeClass('mgnb_on');
+        mNewReleases.removeClass('mNew_on');
+    })
+
+    // mobile NewReleases event
+    $('.mgnb .mgnb_list > li:first-child a, .mNew_Releases button').click(function () {
+        mNewReleases.toggleClass('mNew_on');
     })
 })
--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
-// main swiper script
-let swiper = new Swiper(".shoesSlider", {
+$(function () {
+  // main swiper script
+
+  let swiper = new Swiper(".shoesSlider", {
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
     slidesPerView: 3,
     spaceBetween: 30,
     freeMode: true,
+    clickable: true,
     scrollbar: {
       el: ".swiper-scrollbar",
-    //   clickable: true,
     },
+    breakpoints: {
+      960: {
+        slidesPerView: 3,
+      },
+      640: {
+        slidesPerView: 2.2,
+      },
+      0: {
+        slidesPerView: 1.2,
+      }
+    }
   });
+
+  // 640px section img change
+  $(window).resize(function () {
+    let wWidth = $(window).width()
+    if (wWidth <= 640) {
+      $('section:nth-child(1) img').attr('src', "./img/main/mobile_main_banner.gif")
+      $('section:nth-child(2) video').attr('src', "./img/main/mobile_section2_banner.mp4")
+      $('section:nth-child(3) img').attr('src', "./img/main/mobile_section3_img.gif")
+      $('section:nth-child(4) img').attr('src', "./img/main/mobile_section4_img.jpg")
+      $('section:nth-child(5) img').attr('src', "./img/main/mobile_section5_img.jpg")
+    } else {
+      $('section:nth-child(1) img').attr('src', "./img/main/main_banner.gif");
+      $('section:nth-child(2) video').attr('src', "./img/main/section2_banner.mp4")
+      $('section:nth-child(3) img').attr('src', "./img/main/section3_img.gif")
+      $('section:nth-child(4) img').attr('src', "./img/main/section4_img.jpg")
+      $('section:nth-child(5) img').attr('src', "./img/main/section5_img.jpg")
+    }
+  }).resize();
+
+
+
+}) // document ready
