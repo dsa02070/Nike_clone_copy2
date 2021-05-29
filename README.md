0ebc6325020c5d6b52148d01a2eec830b959582e
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-30 01:25:20 +0900

    code remove
    불필요한 코드 제거
    추가 링크 연결

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Nike_clone_copy
- 나이키 홈페이지 클론카피
+## Nike_clone_copy
+
+가. 사용된 언어
+ 1. html
+ 2. css
+ 3. jquery
+ 
+나. 컨텐츠 구분
+ * 쇼핑몰 홈페이지
+ 
+다. 형태
+ * 반응형 / pc - tablet - mobile
+ 
+라. 구성
+ 1. 메인페이지 1페이지
+ 2. 서브페이지 6페이지
+
+[나이키 기존 홈페이지 링크]
+(https://www.nike.com/kr/ko_kr/)
--- js/wishList.js
+++ js/wishList.js
@@ -3,18 +3,19 @@ $(function () {
     $('.lnb_button').click(function () {
         $('.lnb').toggleClass('on')
     })
-
     $('.list_close').click(function () {
         $(this).parents('li').css("display", 'none');
     })
 
-    let conList = $('.content_list ul li')
-    conList.each(function () {
-        let b = $(this).index() + 1;
-        console.log(b)
-        let c = $('.content_list h2 em').text(b)
+    //lnb event
+    $(window).scroll(function () {
+        let scTop = $(this).scrollTop()
+        console.log(scTop)
+        if (scTop >= 33) {
+            $('.lnb').addClass("Top_on")
+        } else {
+            $('.lnb').removeClass('Top_on')
+        }
     })
-    let d = conList.index();
-    console.log(d)
 
 }) // ducument ready
