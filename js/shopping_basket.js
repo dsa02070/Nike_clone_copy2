6e1e6ffa4a8371c042bafc4b51f8e8534ed34b59
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-31 12:18:08 +0900

    update
    update

--- js/shopping_basket.js
+++ js/shopping_basket.js
@@ -1,3 +1,31 @@
 $(function () {
-
+    // shopping basket recomend_product_slider
+    let swiper = new Swiper(".recomendSlider", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        clickable: true,
+        slidesPerGroup: 5,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            960: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+            640: {
+                slidesPerView: 3.2,
+                slidesPerGroup: 3.2,
+            },
+            475: {
+                slidesPerView: 2.2,
+                slidesPerGroup: 2.2,
+            },
+            0: {
+                slidesPerView: 1.2,
+                slidesPerGroup: 1.2,
+            }
+        }
+    });
 })
